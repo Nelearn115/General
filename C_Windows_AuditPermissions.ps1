3e349be730bdc4e84b703a0805e5a940373ab65a
Nelearn115 <kartshifter66@gmail.com>
2024-12-06 15:40:31 -0500

    Add files via upload

---
+++ C_Windows_AuditPermissions.ps1
@@ -0,0 +1,16 @@
+﻿
+$acl = Get-Acl -Path "C:\Windows"
+
+$auditRule = New-Object System.Security.AccessControl.FileSystemAuditRule(
+    "Everyone", 
+    "FullControl", 
+    "None",
+    "None", 
+    "Success,Failure"
+)
+
+$acl.AddAuditRule($auditRule)
+
+Set-Acl -Path "C:\Windows" -AclObject $acl
+
+#############################################################################
---
+++ Run_RunOnce_AuditPermissions.ps1
@@ -0,0 +1,24 @@
+﻿
+$auditRule = New-Object System.Security.AccessControl.RegistryAuditRule(
+    "Everyone", 
+    "FullControl", 
+    "None",
+    "None", 
+    "Success,Failure"
+)
+
+#############################################################################
+
+$acl = Get-Acl -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+
+$acl.SetAuditRule($auditRule)
+
+Set-Acl -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -AclObject $acl
+
+#############################################################################
+
+$acl = Get-Acl -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+
+$acl.SetAuditRule($auditRule)
+
+Set-Acl -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -AclObject $acl
